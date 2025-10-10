9fc0f6c192da34c9609637ec8834a561d41c97d2
tamakou <tamakou1217@gmail.com>
2025-10-11 00:50:23 +0900

    Fix 3D reset to preserve target size and guard invalid images

--- Zeus_PC_UI-main/AppUI/AppUI/Core/ThProc.cs
+++ Zeus_PC_UI-main/AppUI/AppUI/Core/ThProc.cs
@@ -1539,6 +1539,64 @@ public partial class ThProc
         return UpdateImagePanChanged(volInfo.VolumeInfo.volID, volInfo.RenderImageInfo3D, deltaX, deltaY, false);
     }
 
+    public static WriteableBitmap? ResetImageViewFor2D(int volumeInfoKey)
+    {
+        if (!VolumeRenderInfos.TryGetValue(volumeInfoKey, out var volInfo) ||
+            volInfo.RenderImageInfo2D == null)
+        {
+            TryGetValueErrorLog("ResetImageViewFor2D", volumeInfoKey);
+            var errMsg = MessageService.GetMessage(MessageCode.VolumeKeyUndefined);
+            throw new WarningException("00000", errMsg);
+        }
+
+        return ResetImageView(volInfo.VolumeInfo.volID, volInfo.RenderImageInfo2D, true);
+    }
+
+    public static WriteableBitmap? ResetImageViewFor3D(int volumeInfoKey)
+    {
+        if (!VolumeRenderInfos.TryGetValue(volumeInfoKey, out var volInfo) ||
+            volInfo.RenderImageInfo3D == null)
+        {
+            TryGetValueErrorLog("ResetImageViewFor3D", volumeInfoKey);
+            var errMsg = MessageService.GetMessage(MessageCode.VolumeKeyUndefined);
+            throw new WarningException("00000", errMsg);
+        }
+
+        var renderInfo = volInfo.RenderImageInfo3D;
+        var currentRenderGc = renderInfo.RenderGC;
+
+        var resetImage = ResetImageView(volInfo.VolumeInfo.volID, renderInfo, false);
+
+        // 同期されたRenderGCを取得し、DLL側で更新された回転情報を保持する
+        if (!UpdateRenderGraphicContext(renderInfo))
+        {
+            Log.Warn("ResetImageViewFor3D UpdateRenderGraphicContext failed. volId:{0}", volInfo.VolumeInfo.volID);
+            return resetImage;
+        }
+
+        var refreshedRenderGc = renderInfo.RenderGC;
+
+        if (currentRenderGc.targeSizeX > 0)
+        {
+            refreshedRenderGc.targeSizeX = currentRenderGc.targeSizeX;
+        }
+
+        if (currentRenderGc.targeSizeY > 0)
+        {
+            refreshedRenderGc.targeSizeY = currentRenderGc.targeSizeY;
+        }
+
+        refreshedRenderGc.panX = 0;
+        refreshedRenderGc.panY = 0;
+        refreshedRenderGc.zoom = 1.0f;
+        refreshedRenderGc.renderCmdMinor = (int)RenderingCmdMinorType.None;
+        renderInfo.RenderGC = refreshedRenderGc;
+
+        var refreshedImage = UpdateImage(volInfo.VolumeInfo.volID, renderInfo, false);
+
+        return refreshedImage ?? resetImage;
+    }
+
     private static WriteableBitmap? UpdateImagePanChanged(int volId, ThRenderImageInfo renderInfo, double deltaX, double deltaY, bool is2D)
     {
         var renderGc = renderInfo.RenderGC;
@@ -1561,6 +1619,36 @@ public partial class ThProc
         return retImage;
     }
 
+    private static WriteableBitmap? ResetImageView(int volId, ThRenderImageInfo renderInfo, bool is2D)
+    {
+        var renderGc = renderInfo.RenderGC;
+
+        renderGc.renderCmdMajor = is2D ? (int)RenderingCmdType.Dim2 : (int)RenderingCmdType.Dim3;
+        renderGc.renderCmdMinor = (int)RenderingCmdMinorType.Reset;
+        renderGc.panX = 0;
+        renderGc.panY = 0;
+        renderGc.zoom = 1.0f;
+
+        renderInfo.RenderGC = renderGc;
+
+        var retImage = UpdateImage(volId, renderInfo, is2D);
+        if (retImage == null)
+        {
+            Log.Warn("ResetImageView UpdateImage(volId:{0}, is2D:{1}) is null.", volId, is2D);
+            GetLastError(out var errCode, out var errMessage);
+            throw new WarningException(errCode, errMessage);
+        }
+
+        var updatedRenderGc = renderInfo.RenderGC;
+        updatedRenderGc.renderCmdMinor = (int)RenderingCmdMinorType.None;
+        updatedRenderGc.panX = 0;
+        updatedRenderGc.panY = 0;
+        updatedRenderGc.zoom = 1.0f;
+        renderInfo.RenderGC = updatedRenderGc;
+
+        return retImage;
+    }
+
     private static WriteableBitmap? Get2DImageData(CsThProcLibImage2DData imageData)
     {
         var formatByteSize = (RenderImageFormat)imageData.format switch
@@ -1580,6 +1668,12 @@ public partial class ThProc
             RenderImageFormat.ARGB32 => PixelFormats.Bgra32,
             _ => PixelFormats.Gray8,
         };
+
+        if (imageData.sizeX <= 0 || imageData.sizeY <= 0)
+        {
+            Log.Warn("Get2DImageData received invalid image size. imageID:{0} sizeX:{1} sizeY:{2}", imageData.imageID, imageData.sizeX, imageData.sizeY);
+            return null;
+        }
 
         if (Get2DImageData(imageData.imageID, imageData.sizeX * imageData.sizeY * formatByteSize, out var outBuffer) == 0)
         {
